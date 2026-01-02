328328d8a6d52936d1cbc9ba9f4cac5bc18461fb
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2026-01-02 15:51:06 -0500

    rolledback the namespace and applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.tuneurl.KSALradioapp"
+    namespace = "com.tuneurlradio.app"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.tuneurl.KSALradioapp"
+        applicationId = "com.tuneurlradio.app"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
